471d3245d66c3478600f4337b49eda4886049cd8
Chris Birk <cmbirk@gmail.com>
2022-08-09 14:06:53 -0500

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Script
+    strategy="lazyOnload"
+    src={`https://www.googletagmanager.com/gtag/js?id=UA-119741426-1`}
+  />
+  <Script
+    id="google-analytics"
+    strategy="lazyOnload"
+  >
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-119741426-1');
+    `}
+</Script>
+
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp

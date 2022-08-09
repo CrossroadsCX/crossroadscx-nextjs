ee9721d2c8bff69c34b68157c6a0ee4df5cc8ec5
Chris Birk <cmbirk@gmail.com>
2022-08-09 05:08:03 -0500

    add infographic & koozies redirects

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/infographic',
+        destination: '/',
+        permanent: false,
+      },
+      {
+        source: '/koozies',
+        destination: '/',
+        permanent: false,
+      }
+    ];
+  },
   experimental: {
     images: { allowFutureImage: true },
   },
